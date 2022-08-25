5e34d024a25a2e77e80eba8707e21d6704a4ad1f
Mariya Gubina <batanova.masha@yandex.ru>
2022-08-25 19:10:31 +0700

    Add file Person

---
+++ src/ООР/Main_Person.java
@@ -0,0 +1,33 @@
+package ООР;
+
+import ООР.Person;
+
+import java.util.Scanner;
+
+public class Main_Person {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите имя: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Введите отчество: ");
+        String middleName = scanner.nextLine();
+
+        System.out.print("Введите фамилию: ");
+        String familyName = scanner.nextLine();
+
+        System.out.print("Введите возраст: ");
+        int age = scanner.nextInt();
+
+        Person person = new Person(name, middleName, familyName, age);
+
+        System.out.println("Объект класса Person был создан: "+ person);
+
+        System.out.println("Год рождения: " + getYearOfBirth(person));
+    }
+
+    private static int getYearOfBirth(Person person){
+        return 2022 - person.getAge();
+    }
+}
---
+++ src/ООР/Person.java
@@ -0,0 +1,52 @@
+package ООР;
+
+public class Person {
+    private String name;
+    private String middleName;
+    private String familyName;
+    private int age;
+
+    public Person(String name, String middleName, String familyName, int age) {
+        this.name = name;
+        this.middleName = middleName;
+        this.familyName = familyName;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName) {
+        this.familyName = familyName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + familyName + " " + name + " " + middleName + "}";
+    }
+}
